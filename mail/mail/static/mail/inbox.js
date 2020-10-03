e56c5632ec062a883dc9afa30eb66f9b4533314a
Aaron <Pennwisedom@gmail.com>
2020-10-03 13:38:46 -0400

    Began work on individual Email view page.

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,11 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  
+
+  // Listening for various clicks
   document.querySelector('#compose-form').addEventListener('submit', send_email);
+
+  
  
   // By default, load the inbox
   load_mailbox('inbox');
+
 });
+
 
 function compose_email() {
 
@@ -34,20 +41,35 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-if (mailbox === 'sent') {
+// Sent and Inbox Views
+if (mailbox === 'sent' || 'inbox') {
   fetch(`/emails/${mailbox}`)
-.then(response => response.json())
-.then(emails => {
+  .then(response => response.json())
+  .then(emails => {
     // Print emails
     console.log(emails);
 
-    // ... do something else with emails ...
-});
-}
 
-}
+      emails.forEach( (email, index) => {
+      const element = document.createElement('div');
+      element.setAttribute("class", "emailline");
+      element.setAttribute("id", `email${index}`)
+      element.setAttribute("data-id", `${emails[index].id}`)
+      if (mailbox === 'sent') {
+      element.innerHTML = `<strong>To ${emails[index].recipients}:</strong> ${emails[index].subject} <span class="time"> ${emails[index].timestamp}`;
+    } else {
+      element.innerHTML = `<strong>From ${emails[index].sender}:</strong> ${emails[index].subject} <span class="time"> ${emails[index].timestamp}`;
+    }
+      document.querySelector('#emails-view').append(element);
+      document.querySelector(`#email${index}`).addEventListener('click', () => load_email(emails[index].id));
 
-  
+    });
+    });
+    
+
+
+  }
+}
 
 
 function send_email() {
@@ -66,4 +88,20 @@ function send_email() {
   });
   load_mailbox('sent');
   return false;
+}
+
+function load_email(email_id) {
+  // Hide the Inboxes
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'block';
+
+fetch(`/emails/${email_id}`)
+.then(response => response.json())
+.then(email => {
+    // Print email
+    console.log(email);
+
+    // ... do something else with email ...
+});
 }
